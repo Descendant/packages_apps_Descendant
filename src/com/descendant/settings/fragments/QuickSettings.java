61e1176a4a88c21010076adf2960c5d14c49a15b
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-08-14 01:02:14 +0000

    QS Tile Icon Accent Toggle [2/2]

--- src/com/descendant/settings/fragments/QuickSettings.java
+++ src/com/descendant/settings/fragments/QuickSettings.java
@@ -2,10 +2,13 @@ package com.descendant.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -14,6 +17,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
+
 import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -21,12 +25,22 @@ import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.descendant.settings.preferences.Utils;
+import com.descendant.settings.preferences.SystemSettingSwitchPreference;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String QS_TILE_TINTING = "qs_tile_tinting_enable";
+    private SwitchPreference mEnableQsTileTinting;
+    private Preference mFileHeader;
+    private static final int REQUEST_PICK_IMAGE = 0;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -37,12 +51,35 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
+        //QS Tile Theme
+        mEnableQsTileTinting = (SwitchPreference) findPreference(QS_TILE_TINTING);
+        mEnableQsTileTinting.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QS_TILE_TINTING_ENABLE, 1) == 1);
+        mEnableQsTileTinting.setOnPreferenceChangeListener(this);
+
         }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceTreeClick(Preference preference) {
+	ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFileHeader) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+             startActivityForResult(intent, REQUEST_PICK_IMAGE);
+             return true;
+         }
+         return super.onPreferenceTreeClick(preference);
+     }
 
-        return false;
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	if (preference == mEnableQsTileTinting) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_TILE_TINTING_ENABLE, value ? 1 : 0);
+             Utils.restartSystemUi(getContext());
+        }
+	return true;
     }
 
     @Override
